af73a28c1669eaf8185ae38990ffcae75ea82e0e
Quotenova <mudassar257eb@gmail.com>
2026-07-05 14:11:50 +0500

    quotes.js

---
+++ quotes.js
@@ -0,0 +1,7 @@
+const quotes = [
+  "Success begins where excuses end.",
+  "Small steps every day create big results.",
+  "Discipline beats motivation.",
+  "Believe in yourself and keep moving.",
+  "Dreams work when you do."
+];
